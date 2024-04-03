bb207462cdba0715eeb16bf04a884d43f78ffc76
havertzz04 <havertzz04@gmail.com>
2024-04-02 21:28:51 -0500

    feat: :sparkles: Exercises Using Lits
    In this python document you'll find some basic python exercises using lists

---
+++ semana2/edades_amigos.py
@@ -0,0 +1,34 @@
+# Definir una lista con la edad de 5 amigos
+lista_amigos = [20, 17, 30, 23, 26]
+
+# Agregar la edad de un nuevo amigo
+lista_amigos.append(21)
+
+# Sumar la edad de todos
+print(sum(lista_amigos))
+
+'<----- Tarea de la clase: ----->'
+
+'''Definir una lista de valores de materiales escolares
+
+    1. Agregar 3 elementos a la lista
+    2. Sumar el total de la compra
+    3. Agregar un ultimo elemento $500
+    4. Sumar el total nuevamente
+'''
+
+# Lista con 3 elemntos tipo interger
+precios_materiales_escolares = [6800, 2000, 3700]
+
+# Imprimimos la suma de los elementos de la lista precios_materiales_escolares
+print(sum(precios_materiales_escolares))
+
+# Agregamos un ultimo elemnto tipo interger (500)
+precios_materiales_escolares.append(500)
+
+# Imprimimos nuevamente la suma de los elementos de la lista precios_materiales_escolares
+print(sum(precios_materiales_escolares))
+
+
+
+
