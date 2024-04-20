1cee927f9770a66b713793a713e0c9ed981512b5
havertzz04 <havertzz04@gmail.com>
2024-04-19 19:55:52 -0500

    feat: :rocket: Completed Code
    I completed all the validations necessaries and I close any loop pending on the code lol, so now this program works alone and there is not any strange loop

--- semana4/clase2/decoradores.py
+++ semana4/clase2/decoradores.py
@@ -1,76 +1,78 @@
 def display_menu():
-    user_menu = {
-        1 : "1. Numbers from 1 to N",
-        2 : "2. Even numbers",
-        3 : "3. Odd numbers"
-    }
-       
-    method_menu = {
-        1 : display_numbers,
-        2 : even_number,
-        3 : odd_number
-    }
-   
-    print("\n<---This is our numbers menu--->\n")
-    for option in user_menu.values():
+    user_menu = ["1. Numbers from 1 to N", "2. Even numbers", "3. Odd numbers"]   
+    method_menu = [display_numbers, even_number, odd_number]
+
+    print("\n<--------This is our options menu-------->\n")
+    for option in user_menu:
         print(option)
-                    
-    answer = int(input("\nSelect your method option here: "))
-    if answer > 0 and answer < 4:
-        method_menu[answer]()
-    else:
-        print("Invalid option. Please choose a valid option.")
+    
+    while True:                
+        answer = int(input("\nSelect your option here: "))
+        if 0 < answer < 4:
+            method_menu[answer - 1]()
+            break
+        else:
+            print("Please select a valid option (1 to 3).")
 
 
 #Decorated Function
 def display_exit(func):
     def wrapper():
         func()
-        exit_menu = {
-            1 : "1. Go to the options menu",
-            2 : "2. Exit"
-        }
+        exit_menu = ["1. Go to the options menu", "2. Exit"]
     
-        print("\n<---Select the best option for you--->\n")
-        for option in exit_menu.values():
-            print(option)
-        
-        exit_answer = int(input("\nSelect your option here: "))   
-        if exit_answer == 1:
-            display_menu()     
-        elif exit_answer == 2:
-            print("\nThank you!")    
-        else:
-            print("Invalid option. Please choose a valid option.")
-        
+        while True:
+            print("\n<---Select the best option for you--->\n")
+            for option in exit_menu:
+                print(option)
+            
+            exit_answer = int(input("\nSelect your option here: "))   
+            if exit_answer == 1:
+                display_menu()
+                break             
+            elif exit_answer == 2:
+                print("\nThank you!")
+                break
+            else:
+                print("Please choose a valid option (1 or 2).")                  
+     
     return wrapper
                  
     
 #Decorated Function
 def positive_number(func):
     def wrapper():
-        answer = int(input("Enter any number: "))
-        func(answer)
+        while True:
+            answer = int(input("Enter any number: "))
+            if answer > 0:
+                func(answer)
+                break
+            else:
+                print("\nPlease enter a positive number.")           
+                    
     return wrapper             
+               
              
-  
 @display_exit                                          
 @positive_number
 def display_numbers(answer):
+    print(f"\nNumbers from 1 to {answer}:")
     for i in range(1, answer + 1):
         print(i)
-  
+   
 @display_exit                                                     
 @positive_number
 def even_number(answer):
+    print(f"\Even numbers up to {answer}:")
     for i in range(2, answer + 1, 2):
         print(i)
-     
+          
 @display_exit                                                       
 @positive_number      
 def odd_number(answer):
+    print(f"\nOdd numbers up to {answer}:")
     for i in range(1, answer + 1, 2):
         print(i)
 
 
-display_menu()
+display_menu() #You only need to use this function and the program will work alone <3
