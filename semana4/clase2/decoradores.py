8c790e3898e71f78b4ee7c8af94ffcb65cbb25e9
havertzz04 <havertzz04@gmail.com>
2024-04-19 19:07:13 -0500

    feat: :zap: Almost Done
    This code is almost done, the validation are working but I will keep editing it in order to making easier to read and I will try to make better validations

--- semana4/clase2/decoradores.py
+++ semana4/clase2/decoradores.py
@@ -1,49 +1,76 @@
-class menu_options:
-    def __init__(self):
-        self.menu = {
-            1 : "1. Numbers from 1 to N.",
-            2 : "2. Even numbers",
-            3 : "3. Odd numbers"
+def display_menu():
+    user_menu = {
+        1 : "1. Numbers from 1 to N",
+        2 : "2. Even numbers",
+        3 : "3. Odd numbers"
+    }
+       
+    method_menu = {
+        1 : display_numbers,
+        2 : even_number,
+        3 : odd_number
+    }
+   
+    print("\n<---This is our numbers menu--->\n")
+    for option in user_menu.values():
+        print(option)
+                    
+    answer = int(input("\nSelect your method option here: "))
+    if answer > 0 and answer < 4:
+        method_menu[answer]()
+    else:
+        print("Invalid option. Please choose a valid option.")
+
+
+#Decorated Function
+def display_exit(func):
+    def wrapper():
+        func()
+        exit_menu = {
+            1 : "1. Go to the options menu",
+            2 : "2. Exit"
         }
     
-    # def display_menu(self, decorated_function):  
-    #     def wrapper():
-    #             for option in self.menu.values():
-    #                 print(option)
-    #                 option += 1
-                    
-    #             answer = int(input("Select your option here: "))
-    #             if answer > 0 and answer < 4:
-    #                 decorated_function
-    #             else:
-    #                 print("Invalid option. Please choose a valid option.")
-
-
-    #     return wrapper()
+        print("\n<---Select the best option for you--->\n")
+        for option in exit_menu.values():
+            print(option)
+        
+        exit_answer = int(input("\nSelect your option here: "))   
+        if exit_answer == 1:
+            display_menu()     
+        elif exit_answer == 2:
+            print("\nThank you!")    
+        else:
+            print("Invalid option. Please choose a valid option.")
+        
+    return wrapper
+                 
     
-    def positive_number(decorated_function):
-        def wrapper(self):
-            answer = int(input("Enter any number: "))
-            decorated_function(self, answer)
-        return wrapper             
-          
-                                  
-    @positive_number
-    def display_numbers(self, answer):
-        for i in range(1, answer + 1):
-            print(i)
-                                                    
-    @positive_number
-    def even_number(self, answer):
-        for i in range(2, answer + 1, 2):
-            print(i)
-                                                     
-    @positive_number      
-    def odd_number(self, answer):
-        for i in range(1, answer + 1, 2):
-            print(i) 
+#Decorated Function
+def positive_number(func):
+    def wrapper():
+        answer = int(input("Enter any number: "))
+        func(answer)
+    return wrapper             
+             
+  
+@display_exit                                          
+@positive_number
+def display_numbers(answer):
+    for i in range(1, answer + 1):
+        print(i)
+  
+@display_exit                                                     
+@positive_number
+def even_number(answer):
+    for i in range(2, answer + 1, 2):
+        print(i)
+     
+@display_exit                                                       
+@positive_number      
+def odd_number(answer):
+    for i in range(1, answer + 1, 2):
+        print(i)
 
 
-menu = menu_options()
-menu.even_number()
-
+display_menu()
